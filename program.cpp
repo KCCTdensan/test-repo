115fced86dce8cdf8d6579c221c1848d715162a0
電算太郎 <kcctdensan@gmail.com>
2017-12-20 17:17:31 +0900

    え?

--- program.cpp
+++ program.cpp
@@ -1,2 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int addition(int a, int b) {
+	return;
+}
+
+int subtraction(int a, int b) {
+	return;
+}
+
+int multiplication(int a, int b) {
+	return;
+}
+
 int main() {
+	int x, y;
+	cin >> x >> y;
+	cout << addition(x, y) << subtraction(x, y) << multiplication(x, y) << endl;
 }
